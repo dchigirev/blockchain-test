e57c478cc5bce85481b15c14c70144be4931400e
Danil Chigirev <danil.chigirev@gmail.com>
2024-08-27 14:47:25 +0700

    feat: platform balance model

---
+++ src/db/models/assets.ts
@@ -0,0 +1,36 @@
+import {
+  Table, PrimaryKey, Column, Model, CreatedAt, UpdatedAt, DeletedAt, DataType, IsUUID,
+} from 'sequelize-typescript';
+
+@Table({
+  paranoid: true,
+  timestamps: true,
+  underscored: true,
+  modelName: 'Client',
+  tableName: 'client',
+})
+export default class Client extends Model<Client> {
+
+  @IsUUID(4)
+  @PrimaryKey
+  @Column
+  public id: string;
+
+  @Column
+  public name: string;
+
+  @Column(DataType.INTEGER)
+  public balance_quote: number;
+
+  @Column(DataType.INTEGER)
+  public balance_tokens: number;
+
+  @CreatedAt
+  public created_at: Date;
+
+  @UpdatedAt
+  public updated_at: Date;
+
+  @DeletedAt
+  public deleted_at: Date;
+}
---
+++ src/db/models/platform-balance.ts
@@ -0,0 +1,33 @@
+import {
+  Table, PrimaryKey, Column, Model, CreatedAt, UpdatedAt, DeletedAt, DataType, IsUUID,
+} from 'sequelize-typescript';
+
+@Table({
+  paranoid: true,
+  timestamps: true,
+  underscored: true,
+  modelName: 'PlatformBalance',
+  tableName: 'platform_balance',
+})
+export default class PlatformBalance extends Model<PlatformBalance> {
+
+  @IsUUID(4)
+  @PrimaryKey
+  @Column
+  public id: string;
+
+  @Column(DataType.DECIMAL)
+  public balance_tokens: number;
+
+  @Column(DataType.DECIMAL)
+  public balance_sol: number;
+
+  @CreatedAt
+  public created_at: Date;
+
+  @UpdatedAt
+  public updated_at: Date;
+
+  @DeletedAt
+  public deleted_at: Date;
+}
